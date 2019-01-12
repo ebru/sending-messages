3fa164fcc3b3684fa7d4fef5ac67b59435c8adb7
Ebru Kaya <ebky14@gmail.com>
2019-01-12 20:35:00 +0300

    base class is added

---
+++ src/model/Base.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+
+namespace Model;
+
+class Base 
+{
+    protected $client;
+    const CLIENT_API_KEY = 'lG7WMSfYQkXM9LImklLsK107L';
+
+    const RESPONSE_STATUS_SUCCESS = 200;
+    const RESPONSE_STATUS_BAD_REQUEST = 400;
+    const RESPONSE_STATUS_UNAUTHORIZED = 401;
+    const RESPONSE_STATUS_GENERAL = 500;
+
+    public function __construct() {
+        $this->client = new \MessageBird\Client(self::CLIENT_API_KEY);
+    }
+}
